7b5a7fe61d01fed9e20007c048877565e75cfea5
Aditya Sinha <adityasinha347@gmail.com>
2025-02-08 15:58:54 +0530

    m

--- Sem4/DAA/Lab/Lab5/q1.c
+++ Sem4/DAA/Lab/Lab5/q1.c
@@ -1,30 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *topo, *visited, idx = 0;
+void dfs(int** graph, int vertex, int n, int* visited, int* topo, int* idx, int* cycleFlag) {
+    if (visited[vertex] == 1) {
+        *cycleFlag = 1;
+        return;
+    }
+    if (visited[vertex] == 2) return;
 
-void dfs(int** graph, int vertex, int n) {
     visited[vertex] = 1;
+
     for (int i = 0; i < n; i++) {
-        if (graph[vertex][i] && !visited[i]) {
-            dfs(graph, i, n);
+        if (graph[vertex][i] && visited[i] != 2) {
+            dfs(graph, i, n, visited, topo, idx, cycleFlag);
         }
     }
-    topo[idx++] = vertex;
+
+    visited[vertex] = 2;
+    topo[(*idx)++] = vertex;
+}
+
+void sourceRemoval(int **graph, int *inDegree, int n) {
+    int *queue = (int*)malloc(n * sizeof(int));
+    int front = 0, rear = 0;
+    int idx = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (inDegree[i] == 0) {
+            queue[rear++] = i;
+        }
+    }
+
+    while (front < rear) {
+        int node = queue[front++];
+
+        printf("%d ", node);
+        idx++;
+
+        for (int i = 0; i < n; i++) {
+            if (graph[node][i] == 1) {
+                inDegree[i]--;
+                if (inDegree[i] == 0) {
+                    queue[rear++] = i;
+                }
+            }
+        }
+    }
+
+    if (idx != n) {
+        printf("\nGraph has a cycle.\n");
+    } else {
+        printf("\n");
+    }
+
+    free(queue);
 }
 
 int main() {
     int n, m;
+    int cycleFlag = 0;
 
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
 
     int **graph = (int**)malloc(n * sizeof(int*));
-    visited = (int*)malloc(n * sizeof(int));
-    topo = (int*)malloc(n * sizeof(int));
+    int *visited = (int*)malloc(n * sizeof(int));
+    int *topo = (int*)malloc(n * sizeof(int));
+    int *inDegree = (int*)malloc(n * sizeof(int));
+    int idx = 0;
 
     for (int i = 0; i < n; i++) {
         graph[i] = (int*)malloc(n * sizeof(int));
+        inDegree[i] = 0;
     }
 
     for (int i = 0; i < n; i++) {
@@ -41,26 +88,41 @@ int main() {
         int u, v;
         scanf("%d %d", &u, &v);
         graph[u][v] = 1;
+        inDegree[v]++;
     }
 
     for (int i = 0; i < n; i++) {
-        if (!visited[i]) {
-            dfs(graph, i, n);
+        visited[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == 0) {
+            dfs(graph, i, n, visited, topo, &idx, &cycleFlag);
+            if (cycleFlag) break;
         }
     }
 
-    printf("Topological Sort: ");
-    for (int i = n - 1; i >= 0; i--) {
-        printf("%d ", topo[i]);
+    printf("Topological Sort (DFS): ");
+    if (cycleFlag) {
+        printf("Graph has a cycle.\n");
+    } else {
+        for (int i = n - 1; i >= 0; i--) {
+            printf("%d ", topo[i]);
+        }
+        printf("\n");
     }
-    printf("\n");
+
+    printf("Topological Sort (Source Removal Method): ");
+    sourceRemoval(graph, inDegree, n);
 
     for (int i = 0; i < n; i++) {
         free(graph[i]);
     }
     free(graph);
+    free(inDegree);
     free(visited);
     free(topo);
 
     return 0;
 }
+
